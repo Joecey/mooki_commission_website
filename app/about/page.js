e61c6b9212d7fdfe931d2b7d0ec5fc6381077ebd
Joe Linogao <linogaoj@tcd.ie>
2023-03-27 22:11:36 +0100

    added new font and added template text

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,15 @@
 export default function AboutPage() {
 	return (
 		<div>
-			<h1>This is the about page</h1>
+			<h1>Hey there!</h1>
+			<p>
+				My name is Harley and I'm a digital artist currently studying
+				Concept art, environment art and character art at university!
+				I've been drawing ever since I could hold a pencil. I've always
+				been interested in art and currently have a level 3 diploma in
+				Graphics, Illustration and Game art as of 2022 graduating
+				college!
+			</p>
 		</div>
 	);
 }
